5814f6c7e79796d334bb5e2190472186a8447b18
fedi.gmd <gammoudifedi@gmail.com>
2020-08-12 16:15:02 +0100

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import { ADD_TODO, TOGGLE_TODO } from './actionTypes'
+
+let nextID = 0
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  id: nextID++,
+  text
+})
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  id
+})
